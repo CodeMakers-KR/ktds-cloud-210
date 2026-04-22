01926cda3e9a0de02a431f041813e38fa60a2c11
장민창 <codemakers.kr@gmail.com>
2026-04-22 21:01:02 +0900

    회원 목록 조회

--- user-service-api/src/main/java/com/example/userservice/service/UserService.java
+++ user-service-api/src/main/java/com/example/userservice/service/UserService.java
@@ -1,5 +1,7 @@
 package com.example.userservice.service;
 
+import java.util.List;
+
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
 import org.springframework.transaction.annotation.Transactional;
@@ -25,6 +27,10 @@ public class UserService {
 		}
 		
 		return null;
+	}
+
+	public List<ResponseUserVO> fetchAllUsers() {
+		return this.userDao.selectAllUsers();
 	}
 
 }
