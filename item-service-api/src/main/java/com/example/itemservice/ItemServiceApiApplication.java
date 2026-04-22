57d7a6d303c7cf8e9d68e116be2318ac0c2f8844
장민창 <codemakers.kr@gmail.com>
2026-04-22 19:53:17 +0900

    MSA Service 생성 중

---
+++ item-service-api/src/main/java/com/example/itemservice/ItemServiceApiApplication.java
@@ -0,0 +1,13 @@
+package com.example.itemservice;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ItemServiceApiApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(ItemServiceApiApplication.class, args);
+	}
+
+}
